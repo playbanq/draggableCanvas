36aa3e2fc288a384b9e341aa358cad4b8029d6a8
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-30 22:37:58 -0500

    Basic drag and drog functionality without key detection.

--- draggableCanvas.js
+++ draggableCanvas.js
@@ -16,18 +16,53 @@ function draggableCanvas(canvas, outerCanvas) {
     }
 
     // Track cursor position
-    var cursor = { x: undefined, y: undefined };
+    var cursor = { x: undefined, y: undefined },
+        mouseDown = false,
+        onDropCallback;
 
     // Define the canvas object interface
     var properties = {
         onDrag: {
-            value: function (callback) {
-            
+            value: function (keyCode, callback) {
+                var dragKeyDown = false;
+                if (typeof keyCode === 'function') {
+                    callback = keyCode;
+                    dragKeyDown = true;
+                }
+
+                document.addEventListener('onkeydown', function (event) {
+                    console.log(event.keyCode);
+                });
+
+                canvas.addEventListener('mousedown', function (event) {
+                    var container = canvas.getBoundingClientRect();
+                    cursor.x = event.clientX;
+                    cursor.y = event.clientY;
+                    mouseDown = true;
+                });
+                document.addEventListener('mousemove', function (event) {
+                    var container = canvas.getBoundingClientRect(),
+                        currentX = event.clientX, 
+                        currentY = event.clientY,
+                        marginTop = parseInt(canvas.style.top) || 0,
+                        marginLeft = parseInt(canvas.style.left) || 0;
+                    if (dragKeyDown && mouseDown) {
+                        canvas.style.top = Math.floor(currentY - cursor.y) + 'px';
+                        canvas.style.left = Math.floor(currentX - cursor.x) + 'px';
+                        // callback();
+                    }
+                });
+                document.addEventListener('mouseup', function (event) {
+                    mouseDown = false;
+                    if (typeof onDropCallback === 'function') {
+                        onDropCallback(event.clientX, event.clientY);
+                    }
+                });
             }      
         },
         onDrop: {
             value: function (callback) {
-            
+                onDropCallback = callback;
             }      
         },
         setSize: {
