f4bc20389ed1a914768554459279636160961052
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-31 00:22:40 -0500

    Added shift key detection as a trigger for dragging.

--- draggableCanvas.js
+++ draggableCanvas.js
@@ -18,25 +18,51 @@ function draggableCanvas(canvas, outerCanvas) {
     // Track cursor position
     var cursor = { x: undefined, y: undefined },
         mouseDown = false,
+        dragKeyDown = false,
+        alwaysDraggable = false,
         onDropCallback;
 
     // Define the canvas object interface
     var properties = {
         onDrag: {
-            value: function (keyCode, callback) {
-                var dragKeyDown = false;
-                if (typeof keyCode === 'function') {
-                    callback = keyCode;
-                    dragKeyDown = true;
-                }
+            value: function (options, callback) {
+                var dragKeyCode;
 
-                document.addEventListener('onkeydown', function (event) {
-                    console.log(event.keyCode);
+                if (typeof options === 'function') {
+                    callback = options;
+                    dragKeyCode = 16;
+                } else if (options === 0) {
+                    alwaysDraggable = true;
+                    dragKeyDown = true;
+                    canvas.style.cursor = 'move';
+                } 
+
+                document.addEventListener('keydown', function (event) {
+                    if (event.keyCode === dragKeyCode) {
+                        var marginTop = parseInt(canvas.style.top) || 0,
+                            marginLeft = parseInt(canvas.style.left) || 0;
+
+                        cursor.x = event.clientX - marginLeft;
+                        cursor.y = event.clientY - marginTop;
+
+                        canvas.style.cursor = 'move';
+                        dragKeyDown = true;
+                    }
+                });
+                document.addEventListener('keyup', function (event) {
+                    if (event.keyCode === dragKeyCode) {
+                        canvas.style.cursor = 'auto';
+                        dragKeyDown = false;
+                    }
                 });
 
                 canvas.addEventListener('mousedown', function (event) {
                     var marginTop = parseInt(canvas.style.top) || 0,
                         marginLeft = parseInt(canvas.style.left) || 0;
+
+                    if (dragKeyDown) {
+                        canvas.style.cursor = 'move';
+                    }
 
                     cursor.x = event.clientX - marginLeft;
                     cursor.y = event.clientY - marginTop;
@@ -49,11 +75,14 @@ function draggableCanvas(canvas, outerCanvas) {
                     if (dragKeyDown && mouseDown) {
                         canvas.style.top = Math.floor(currentY - cursor.y) + 'px';
                         canvas.style.left = Math.floor(currentX - cursor.x) + 'px';
-                        // callback();
+                        callback(canvas.style.top, canvas.style.left, currentX, currentY);
                     }
                 });
                 document.addEventListener('mouseup', function (event) {
                     mouseDown = false;
+                    if (!alwaysDraggable) {
+                        canvas.style.cursor = 'auto';
+                    }
                     if (typeof onDropCallback === 'function') {
                         onDropCallback(event.clientX, event.clientY);
                     }
